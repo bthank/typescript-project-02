3d93857d2de6978ded926ae3bf6ee2185d1a1ae0
bthank <bthankachan@yahoo.com>
2021-07-15 23:26:46 -0400

    added 2nd subclass functionality

--- Driver.js
+++ Driver.js
@@ -2,7 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Shape_1 = require("./Shape");
 var Circle_1 = require("./Circle");
+var Rectangle_1 = require("./Rectangle");
 var myShape = new Shape_1.Shape(10, 5);
-console.log(myShape.getInfo());
+console.log("Shape is: " + myShape.getInfo());
 var myCircle = new Circle_1.Circle(10, 5, 3);
-console.log(myCircle.getInfo());
+console.log("Circle is: " + myCircle.getInfo());
+var myRectangle = new Rectangle_1.Rectangle(10, 20, 75, 90);
+console.log("Rectangle is: " + myRectangle.getInfo());
--- Driver.ts
+++ Driver.ts
@@ -1,8 +1,12 @@
 import {Shape} from "./Shape";
 import {Circle} from "./Circle";
+import {Rectangle} from "./Rectangle";
 
 let myShape = new Shape(10,5);
-console.log(myShape.getInfo());
+console.log("Shape is: " + myShape.getInfo());
 
 let myCircle = new Circle(10, 5, 3);
-console.log(myCircle.getInfo());
+console.log("Circle is: " + myCircle.getInfo());
+
+let myRectangle = new Rectangle(10,20,75,90);
+console.log("Rectangle is: " + myRectangle.getInfo());
